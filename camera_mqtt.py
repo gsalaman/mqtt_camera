31753082102b242ccd2bfbe46dd58e1a63469016
gsalaman <43499190+gsalaman@users.noreply.github.com>
2019-12-06 06:22:20 -0700

    add shutdown command processing.

--- camera_mqtt.py
+++ camera_mqtt.py
@@ -5,26 +5,31 @@ import paho.mqtt.client as mqtt
 from picamera import PiCamera
 
 camera = PiCamera()
-camera.resolution = (64,64)
+camera.resolution = (160,96)
 
 def take_picture():
   global camera
-  camera.capture('temp.jpg')
+  camera.capture("temp.jpg")
 
 # message handling callback 
 def on_message(client, userdata, message):
+  if message.topic == "shutdown":
+    exit(0)
+    
   if message.payload == "displayReady":
     print "ready received.  Storing picture"
     take_picture()
     print "sending imageDone"
     client.publish("camera", "imageDone")
 
-broker_address="10.0.0.17"
+broker_address="mqttbroker"
 client = mqtt.Client("camera")
 client.on_message=on_message
 client.connect(broker_address)
 client.loop_start()
 client.subscribe("camera")
+client.subscribe("shutdown")
+
 print "camera running."
 while True:
   time.sleep(1)
