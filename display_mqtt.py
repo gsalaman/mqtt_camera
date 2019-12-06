d3d75c08497ca9da7192f9d1fc105aa1a5ea7c24
gsalaman <43499190+gsalaman@users.noreply.github.com>
2019-12-06 06:29:48 -0700

    Add shutdown processing

--- display_mqtt.py
+++ display_mqtt.py
@@ -8,12 +8,12 @@ from rgbmatrix import RGBMatrix, RGBMatrixOptions
 from PIL import Image, ImageDraw
 
 # this is the size of ONE of our matrixes. 
-matrix_rows = 64 
-matrix_columns = 64 
+matrix_rows = 32
+matrix_columns = 32
 
 # how many matrixes stacked horizontally and vertically 
-matrix_horizontal = 1 
-matrix_vertical = 1
+matrix_horizontal = 5
+matrix_vertical = 3
 
 total_rows = matrix_rows * matrix_vertical
 total_columns = matrix_columns * matrix_horizontal
@@ -38,18 +38,25 @@ def display_jpg():
 
 # message handling callback 
 def on_message(client, userdata, message):
+  global _shutdown
+  
   if message.payload == "imageDone":
     print "imageDone received.  Displaying picture"
     display_jpg()
     print "sending displayReady"
     client.publish("camera", "displayReady")
-
-broker_address="10.0.0.17"
+  if message.topic == "shutdown":
+    print("received shutdown message");
+    _shutdown = True
+    
+# MAIN CODE
+broker_address="mqttbroker"
 client = mqtt.Client("camera")
 client.on_message=on_message
 client.connect(broker_address)
 client.loop_start()
 client.subscribe("camera")
+client.subscribe("shutdown")
 print "display running."
 print "sending displayReady"
 client.publish("camera", "displayReady")
@@ -57,6 +64,8 @@ client.publish("camera", "displayReady")
 try:
   print("Press CTRL-C to stop")
   while True:
+    if (_shutdown == True):
+      exit(0)
     sleep(1)
 except KeyboardInterrupt:
   exit(0)
